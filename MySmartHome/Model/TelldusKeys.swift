30ed1acc831504886f87d27d14a159eeb68fe34f
BjornAhstrom <44399870+BjornAhstrom@users.noreply.github.com>
2019-11-04 12:36:00 +0100

    Update TelldusKeys.swift

--- MySmartHome/Model/TelldusKeys.swift
+++ MySmartHome/Model/TelldusKeys.swift
@@ -10,23 +10,12 @@ import Foundation
 import OAuthSwift
 
 class TelldusKeys {
-    static let token = "7a6155f5d8977ffbf07c32c5946e2dd505c24b938"
-    static let token_secret = "20322a1cca49b2b58929fde2de38c18e"
+    static let token = ""
+    static let token_secret = ""
     
     static let oauthswift = OAuth1Swift(
-        consumerKey:    "FEHUVEW84RAFR5SP22RABURUPHAFRUNU", //ConsumerKey
-        consumerSecret: "ZUXEVEGA9USTAZEWRETHAQUBUR69U6EF"  //ConsumerSecret
+        consumerKey:    "", //ConsumerKey
+        consumerSecret: ""  //ConsumerSecret
     )
     
 }
-
-
-
-//    let public_key = "FEHUVEW84RAFR5SP22RABURUPHAFRUNU"   //ConsumerKey
-//    let private_key = "ZUXEVEGA9USTAZEWRETHAQUBUR69U6EF"  //ConsumerSecret
-//
-//    let requestToken = "https://api.telldus.com/oauth/requestToken"
-//    let authorizeToken = "https://api.telldus.com/oauth/authorize"
-//    let accessToken = "https://api.telldus.com/oauth/accessToken"
-    
-//
